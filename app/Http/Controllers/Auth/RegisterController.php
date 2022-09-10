8cab284b22133986033001d163857bdfa808a770
Thiago Guidolin <developer.arruda@gmail.com>
2022-09-10 17:34:11 -0300

    feat: dando 3 dias de uso grátis

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,7 +70,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'status' => 'ativo',
-            'mensalidade' => now()->subDay(7),
+            'mensalidade' => now()->subDay(3),
             'level' => 'regular',
             'telegram_id' => null,
             'whatsapp_id' => null,
